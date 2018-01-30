59ce0f4301421f4508d0f36efe0ae1b477c52cd0
John Zhou <john.km.zhou@gmail.com>
2018-01-29 18:52:35 -1000

    Migrate to Cordova.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const startApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+if(window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+} else {
+  startApp();
+}
